5c5a884018e0bd66947d64343c2d3fc3f854bd89
Francois Berthault <fberthault@gmail.com>
2015-01-14 23:23:00 +0100

    update web

--- angular-rest-web/src/main/java/com/fanfansama/web/config/MyJerseyRestAppConfig.java
+++ angular-rest-web/src/main/java/com/fanfansama/web/config/MyJerseyRestAppConfig.java
@@ -1,20 +1,21 @@
 package com.fanfansama.web.config;
 
+
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.web.filter.RequestContextFilter;
 
 
-public class MyJerseyRestAppConfig // extends ResourceConfig
+public class MyJerseyRestAppConfig extends ResourceConfig
 {
  
     /**
     * Register JAX-RS application components.
     */    
     public MyJerseyRestAppConfig(){
-     //   register(RequestContextFilter.class);
-     //   packages("com.fanfansama.web");
-     //   register(JacksonFeature.class);
+        register(RequestContextFilter.class);
+        packages("com.fanfansama.web.*");
+        register(JacksonFeature.class);
    ///   //  registerInstances(new LoggingFilter(Logger.getLogger(MyJerseyRestAppConfig.class.getName()), true));
 
     }
