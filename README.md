d4f428d2ede58f663b1b2e6eac3713df10ef0eee
fanfansama <fberthault@gmail.com>
2015-02-27 14:49:04 +0100

    TF

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ angular-rest
 
 Angular JS 1.2.3 + JQuery + BootStrap
 
-JAX-RS 2.0 API > Jersey Restful WebServic + Servlet 3
+JAX-RS 2.0 API > Jersey Restful WebService + Servlet 3
 
 Spring > IOC FrameWork
 
---
+++ angular-rest-geb/src/test/groovy/org/specs/GoogleWikipediaSpec.groovy
@@ -0,0 +1,35 @@
+package org.specs
+
+import org.pages.GoogleHomePage
+import org.pages.GoogleResultsPage
+import org.pages.WikipediaPage
+
+/**
+ * Created by francois on 27/02/15.
+ */
+class GoogleWikipediaSpec {
+
+    def "first result for wikipedia search should be wikipedia"() {
+        given:
+        to GoogleHomePage
+
+        expect:
+        at GoogleHomePage
+
+        when:
+        search.field.value("wikipedia")
+
+        then:
+        waitFor { at GoogleResultsPage }
+
+        and:
+        firstResultLink.text() == "Wikipedia"
+
+        when:
+        firstResultLink.click()
+
+        then:
+        waitFor { at WikipediaPage }
+    }
+
+}
